306bfb3d959cf93b4a1c2e61ca459c9884c16d4d
AlvaroCabero <alvarocabero@gmail.com>
2021-11-17 18:51:30 +0100

    Added Server and DB main structure

---
+++ index.js
@@ -0,0 +1,15 @@
+const connectDB = require("./database");
+const initializeServer = require("./server");
+
+require("dotenv").config();
+
+const port = process.env.SERVER_PORT ?? 5000;
+
+(async () => {
+  try {
+    await connectDB();
+    initializeServer(5000);
+  } catch (error) {
+    // generalErrorHandler;
+  }
+})();
---
+++ server/index.js
@@ -0,0 +1,49 @@
+require("dotenv").config();
+const express = require("express");
+const debug = require("debug")("tuitah:server");
+const chalk = require("chalk");
+const morgan = require("morgan");
+const cors = require("cors");
+// const {
+//   generalErrorHandler,
+//   notFoundErrorHandler,
+// } = require("./middlewares/errors");
+
+const app = express();
+
+const serveRoutes = () => {
+  app.use(cors());
+  app.use(morgan("dev"));
+  app.use(express.json());
+
+  // app.use("/robots", isAuth, robotsRoutes);
+  // app.use("/user", usersRoutes);
+
+  // app.use(notFoundErrorHandler);
+  // app.use(generalErrorHandler);
+};
+
+const initializeServer = (port) => {
+  console.log(port);
+  new Promise((resolve, reject) => {
+    const server = app.listen(port, () => {
+      debug(chalk.yellow(`Running in port ${port}`));
+    });
+
+    server.on("error", (error) => {
+      debug(chalk.red("Initialize the server has not been possible."));
+      if (error.code === "EADDRINUSE") {
+        debug(chalk.red(`The port ${port} is already being used.`));
+      }
+      reject();
+    });
+
+    server.on("close", () => {
+      debug(chalk.magenta("Servidor desconectado"));
+    });
+    serveRoutes();
+    resolve(server);
+  });
+};
+
+module.exports = initializeServer;
